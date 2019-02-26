9eb2b359c65ea934b6fa6942c4c0a813a2eeb3ff
James Chun <jchun00@stuy.edu>
2019-02-26 10:14:47 -0500

    got it to read file and just need to add to array

--- ReadFile.java
+++ ReadFile.java
@@ -7,21 +7,32 @@ import java.util.Scanner;
 public class ReadFile {
   public static void main(String args[]) throws FileNotFoundException {
 
+
         //instead of a try/catch, you can throw the FileNotFoundException.
         //This is generally bad behavior
 
 
-        File text = new File("input.txt");
+        File text = new File(args[0]);
         // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
 
+        int t=0;
+        Scanner temp = new Scanner(text);
+        while (temp.hasNextLine()){
+          t++;
+        }
+        char[][] list = new char[][inf.nextLine().length()]
 
         //inf stands for the input file
 
         Scanner inf = new Scanner(text);
 
+
         while(inf.hasNextLine()){
             String line = inf.nextLine();
-            System.out.println(line);//hopefully you can do other things with the line
+            //System.out.println(line);//hopefully you can do other things with the line
+            for (int i=0;i<line.length();i++){
+
+            }
         }
     }
 }
