8412e409bd440f149b92ac3d71622b6ac9b25d64
James-Chun <jchun00@stuy.edu>
2019-02-26 23:05:14 -0500

    made more progress - added more variables and made list

--- ReadFile.java
+++ ReadFile.java
@@ -19,20 +19,22 @@ public class ReadFile {
         Scanner temp = new Scanner(text);
         while (temp.hasNextLine()){
           t++;
+          temp.nextLine();
         }
-        char[][] list = new char[][inf.nextLine().length()]
 
         //inf stands for the input file
 
         Scanner inf = new Scanner(text);
+        char[][] list = new char[t][inf.nextLine().length()];
 
-
+        int index=0;
         while(inf.hasNextLine()){
             String line = inf.nextLine();
             //System.out.println(line);//hopefully you can do other things with the line
             for (int i=0;i<line.length();i++){
-
+              list[index][i]= line.charAt(i);
             }
+            index++;
         }
     }
 }
